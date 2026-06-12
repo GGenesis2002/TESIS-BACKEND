3a077de3bed133e4b8e6a832d94bfd69f1562596
genesisoa <genesis.ordoneza@ug.edu.ec>
2026-06-12 10:10:22 -0500

    Migrar SMTP Gmail a Brevo

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 465,
-    secure: true,
+    port: 587,          // ← de 465 a 587
+    secure: false,      // ← de true a false (STARTTLS en lugar de SSL)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -18,7 +18,6 @@ const enviarCorreo = async (to, subject, html) => {
             subject,
             html
         });
-
         console.log('Correo enviado a:', to);
         return true;
     } catch (error) {

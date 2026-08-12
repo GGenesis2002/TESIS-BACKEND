607ea367991b82c08c421966255894d61d76f523
genesisoa <genesis.ordoneza@ug.edu.ec>
2026-08-12 12:10:50 -0500

    Corregir BD

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 require('./jobs/Notificacioncronjob');
 require('./helpers/scheduledTasks'); // ← activa los cron jobs al iniciar el servidor
 const express = require('express');
+
+app.set('trust proxy', 1);
+
+
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
